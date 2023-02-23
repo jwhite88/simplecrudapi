d08d0ec44659e58d8fcaa5e38c2c248935378fa1
jwhite88 <74882417+jwhite88@users.noreply.github.com>
2023-02-23 11:50:31 -0500

    Added starting components for the project.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="h1">Simple CRUD API</div>
+      <div className="h1">Simple CRUD API Using JSON Placeholder</div>
     </div>
   );
 }
